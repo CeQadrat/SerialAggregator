52d2879aa8a5c9497a9895ba8672198a263fe0dc
Dima Naumchik <ceqadrat@gmail.com>
2016-10-06 17:24:40 +0300

    create express server add config.json

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const app = require('./app');
+const config = require('./config/config');
+
+let server = http.createServer(app);
+
+server.listen(config.get('port'));
+server.on('error', (error) => {
+    console.error(error);
+});
+server.on('listening', () => {
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+});
