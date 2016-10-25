1673abb8f465e899757d05924aa2f90033b9cb32
Dima Naumchik <ceqadrat@gmail.com>
2016-10-25 14:24:04 +0300

    add lib

--- app.js
+++ app.js
@@ -26,10 +26,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap-social'));
-app.use('/', express.static(__dirname + '/lib/font-awesome-4.6.3'));
+app.use('/', express.static(__dirname + '/lib'));
 app.use('/js', express.static(__dirname + '/node_modules/angular'));
 
 app.use(cookieParser());
